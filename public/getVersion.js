5ecb75ad19edec4626822ed9e3c59a95dbf2cbeb
Maciej Kotowski <makoteq@gmail.com>
2022-10-25 13:18:03 +0200

    delete unsupported tags

--- public/getVersion.js
+++ public/getVersion.js
@@ -4,7 +4,7 @@ const getVersion = (res,name) => {
     axios
       .get(`https://registry.npmjs.com/-/v1/search?text=${name}`)
       .then(function (response) {
-        res.send(label(response.data.objects[0].package.version));
+        res.send(label(response.data.objects[0].package.version,name));
       })
       .catch(function (error) {
         console.log(error);
--- public/label.js
+++ public/label.js
@@ -1,45 +1,6 @@
 
-const label = (version) => {
-  return `<a href="https://www.npmjs.com/package/${version}"><div
-  style="
-     width: 180px;
-     height: 50px;
-     border: 1px solid #000000;
-     border-radius: 8px;
-   "><div
-  style="
-       float: left;
-       border-radius: 8px 0px 0px 8px;
-       background-color: #fff;
-       width: 90px;
-       height: 50px;
-       position: relative;
-       
-     "><img src="/npm-logo-red.svg" style=" height: 20px;  position: absolute;
-       top: 50%;
-       left: 50%;
-       -ms-transform: translate(-50%, -50%);
-       transform: translate(-50%, -50%);""></span></div><div
-  style="
-       float: left;
-       border-radius: 0px 8px 8px 0px;
-       background-color: #d8413f;
-       width: 90px;
-       height: 50px;
-       position: relative;
-     "><span
-  style="font-family: 'Trebuchet MS';
-         font-style: normal;
-         font-size: 20px;
-         margin: 0;
-         color: #fff;
-         position: absolute;
-         font-weight: bold;
-         top: 50%;
-         left: 50%;
-         -ms-transform: translate(-50%, -50%);
-         transform: translate(-50%, -50%);">${version}</span
-     ></div></div></a>`;
+const label = (version,name) => {
+  return `${version}`;
 };
 
 module.exports = label;
